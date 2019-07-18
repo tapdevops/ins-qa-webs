99870866a4690356d8f4f9bc4620e970c45f4170
ferdinand12345 <ferdshinodas@gmail.com>
2019-07-18 15:18:47 +0700

    Ferdinand

--- tapmi/app/APISetup.php
+++ tapmi/app/APISetup.php
@@ -27,18 +27,18 @@ class APISetup extends Model {
 			"dev" => array(
 				"msa" => array(
 					"ins" => array(
-						// "auth" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-auth",
-						// "hectarestatement" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-hectarestatement",
-						// "finding" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-finding",
-						// "inspection" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-inspection",
-						// "report" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-reports",
-						// "ebccvalidation" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-ebccval"
-						"auth" => "http://app.tap-agri.com/mobileinspection/ins-msa-auth",
+						"auth" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-auth",
 						"hectarestatement" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-hectarestatement",
 						"finding" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-finding",
-						"inspection" => "http://localhost:3010",
+						"inspection" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-inspection",
 						"report" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-reports",
 						"ebccvalidation" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-ebccval"
+						// "auth" => "http://app.tap-agri.com/mobileinspection/ins-msa-auth",
+						// "hectarestatement" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-hectarestatement",
+						// "finding" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-finding",
+						// "inspection" => "http://localhost:3010",
+						// "report" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-reports",
+						// "ebccvalidation" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-ebccval"
 					)
 				)
 			),
