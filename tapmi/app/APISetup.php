8470ae6e6e28bd1f20c32caf3243c05075e81531
ferdinand12345 <ferdshinodas@gmail.com>
2019-08-26 11:00:31 +0700

    Fedinand

--- tapmi/app/APISetup.php
+++ tapmi/app/APISetup.php
@@ -28,12 +28,6 @@ class APISetup extends Model
 			"dev" => array(
 				"msa" => array(
 					"ins" => array(
-						// "auth" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-auth",
-						// "hectarestatement" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-hectarestatement",
-						// "finding" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-finding",
-						// "inspection" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-inspection",
-						// "report" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-reports",
-						// "ebccvalidation" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-ebccval"
 						"auth" => "http://app.tap-agri.com/mobileinspection/ins-msa-auth",
 						"hectarestatement" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-hectarestatement",
 						"finding" => "http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-finding",
