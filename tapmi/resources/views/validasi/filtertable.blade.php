9136fa8add971328b92e5b60814125de2bcb231c
Emanuel Riolan <emanuelriolan@gmail.com>
2020-07-13 19:36:00 +0700

    ok

--- tapmi/resources/views/validasi/filtertable.blade.php
+++ tapmi/resources/views/validasi/filtertable.blade.php
@@ -4,9 +4,14 @@
 		<div class="row">
 			<div class="col-md-4"></div>
 			<div class="col-md-4"></div>
-			<div class="col-md-4 m--align-right">
-			<!-- '.$id.'-'.$q['id_ba'].'-'.$q['id_afd']) ba afd dari session -->
-			@if(!empty($records) && $status == 1)
+				@if(!empty($records) && $status == 1)
+			<div class="col-md-4 m--align-right" style="white-space:nowrap;">
+				<div id="cekaslap" class="btn btn-danger m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill">
+					<span>
+						<i class="fa fa-refresh"></i>
+						<span>Cek Validasi Aslap</span>
+					</span>
+				</div>
 				<a href="{{ URL::to('/validasi/create/'.$tgl_validasi) }}" class="btn btn-focus m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill">
 					<span>
 						<i class="fa fa-clipboard"></i>
@@ -28,17 +33,19 @@
 			<th>Krani Buah</th>
 			<th>Afdeling</th>
 			<th>Mandor Panen</th>
+			<th>BCC berhasil Divalidasi Aslap</th>
 			<th>Jumlah BCC yang Divalidasi</th>
 			<th>Keterangan</th>
 		</tr>
 	</thead>
-<tbody>
+	<tbody>
 		@foreach ( $data_header as $key => $q )
 			<tr>
 				<td>{{ $q['tanggal_rencana'] }}</td>
 				<td>{{ $q['nama_krani_buah'] }} - {{$q['nik_kerani_buah']}}</td>
 				<td>{{ $q['id_afd'] }}</td>
 				<td>{{ $q['nama_mandor'] }} - {{$q['nik_mandor']}}</td>
+				<td>0</td>
 				<td>{{ $q['jumlah_ebcc_validated'] }} / {{ $q['target_validasi'] }}  </td>
 				<?php 
 					$id = str_replace("/",".",$q['id_validasi']);
@@ -53,3 +60,80 @@
 	</tbody>
 </table>
 </div>
+<script>
+	$("#cekaslap").click(function(){
+		cekaslap();
+	});
+	function cekaslap(){
+		var search = document.getElementById('generalSearch').value;
+		var datatable = {
+							init: function() {
+								var e;
+								e = $(".m-datatable").mDatatable({
+									data: {
+										saveState: {
+											cookie: !1
+										},
+										
+										autoColumns: true
+									},
+									search: {
+										input: $( "#generalSearch" )
+									},
+
+
+									columns: [
+										{
+											field: "Tanggal",
+											filterable: true,
+											sortable: false,
+											width: 0,
+											visibility: false,
+										},{
+											field: "Krani Buah",
+											filterable: true,
+											sortable: false,
+											width: 300
+										}, {
+											field: "Afdeling",
+											width: '90px',
+											sortable: false,
+										},{
+											field: "Mandor Panen",
+											width: 300,
+											sortable: false,
+										}, {
+											field: "BCC berhasil Divalidasi Aslap",
+											width: 100,
+											sortable: false,
+										}, {
+											field: "Jumlah BCC yang Divalidasi",
+											width: 100,
+											sortable: false,
+										}, {
+											field: "Keterangan",
+											width: 100,
+											sortable: false,
+										}
+									]
+
+								})
+							}
+						};
+		// event.preventDefault();
+		const CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+		$.ajax({
+			url:'/getNewdata2',
+			type:'get',
+			data:{
+				CSRF_TOKEN,
+				'tanggal' : search
+			},
+			success:function(data){
+				$("div#table").html(data);
+				datatable.init();
+				$("#generalSearch").val(search);
+			}
+		})
+	}
+</script>
