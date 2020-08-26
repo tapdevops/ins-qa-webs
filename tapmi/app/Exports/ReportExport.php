b8973d6fe0d97d3679ceb1b341293bef758ed08d
analuthfa <luthfa.ana@gmail.com>
2020-08-26 14:37:26 +0700

    exportable

--- tapmi/app/Exports/ReportExport.php
+++ tapmi/app/Exports/ReportExport.php
@@ -6,7 +6,7 @@ namespace App\Exports;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
-use Maatwebsite\Excel\Concerns\Exportable;
+// use Maatwebsite\Excel\Concerns\Exportable;
 
 class ReportExport implements FromView
 {
