d0301a8e24852caa93b10d3128490c73523af35a
analuthfa <luthfa.ana@gmail.com>
2020-08-27 15:45:36 +0700

    dt

--- tapmi/app/Http/Controllers/UserController.php
+++ tapmi/app/Http/Controllers/UserController.php
@@ -247,15 +247,12 @@ class UserController extends Controller {
 		foreach(array_chunk($data, count($data)/50) as $dt){
 				// $results['master_user'] =  json_decode($dt,true);
 				// dd($result['data']== null);
-				foreach($dt as $k=>$dl)
-  				{
-					$results['master_user'] =  json_decode(json_encode($dl), true);
+					$results['master_user'] =  json_decode(json_encode($dt), true);
 					Excel::create('Data User', function ($excel) use ($results) {
 						$excel->sheet( 'Data User', function( $sheet ) use ( $results ) {
 						$sheet->loadView( 'report.list_user_export', $results );
 							} );
 						} )->export( 'xlsx' );
-				}
 		}
 	
 	}
